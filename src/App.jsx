799a1113b06ca0385a7743a8066dff3bec5e98bc
Gwennael Jean <gwennael.jean@gmail.com>
2020-03-04 23:08:01 +0100

    feat: préparation de la topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import {Router, Route, Link} from "react-router-dom";
+import {Router, Route} from "react-router-dom";
 import {connect} from "react-redux";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import history from './history';
 
 import HomeController from './controllers/HomeController';
 import AboutController from './controllers/AboutController';
 
+import Topbar from './containers/Topbar';
+
 class App extends React.Component {
     render() {
         return <div>
+            <CssBaseline />
             <Router history={history}>
+                <Topbar />
                 <Route path="/" exact component={HomeController} />
                 <Route path="/about" exact component={AboutController} />
-                <ul>
-                    <li><Link to="/">Accueil</Link></li>
-                    <li><Link to="/about">A propos</Link></li>
-                </ul>
             </Router>
         </div>;
     }
---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
+
+import {withStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import CachedIcon from '@material-ui/icons/Cached';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import PublishIcon from '@material-ui/icons/Publish';
+
+const styles = theme => ({});
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <AppBar position="sticky">
+                <Toolbar>
+                    <Button component={Link} to="/" color="inherit">Home</Button>
+                    <Button component={Link} to="/about" color="inherit">About</Button>
+
+                    <Tooltip title="Fetch">
+                        <IconButton color="inherit">
+                            <CachedIcon />
+                        </IconButton>
+                    </Tooltip>
+
+                    <Tooltip title="Pull">
+                        <IconButton color="inherit">
+                            <GetAppIcon />
+                        </IconButton>
+                    </Tooltip>
+
+                    <Tooltip title="Push">
+                        <IconButton color="inherit">
+                            <PublishIcon />
+                        </IconButton>
+                    </Tooltip>
+                </Toolbar>
+            </AppBar>
+        );
+    }
+}
+
+Navbar.propTypes = {
+    currentBranch: PropTypes.string.isRequired,
+}
+
+export default withStyles(styles, { withTheme: true })(Navbar);
