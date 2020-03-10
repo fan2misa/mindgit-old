e32258216fba79aa6c5bc965018203d0a49dc186
Gwennael Jean <gwennael.jean@trsb.net>
2020-03-10 09:57:09 +0100

    feat(socket): installation de socket.io

--- back/main.js
+++ back/main.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on *:3000');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
 });
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
 import 'typeface-roboto';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 import App from "./App";
 import createStore from './store';
 import theme from './theme';
 
+import io from 'socket.io-client';
+const socket = io('http://localhost:3000');
 
 let store = createStore();
 
