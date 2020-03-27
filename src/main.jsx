799521524db9eade6eea7c28edff8d4825e91a7d
Gwennael Jean <gwennael.jean@gmail.com>
2020-03-27 17:16:29 +0100

    feat(app): rafraichissement de l'application à la récupération du focus

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
+import { remote } from 'electron';
 
 import './../assets/scss/main.scss';
 import './../node_modules/bootstrap/dist/js/bootstrap';
@@ -8,9 +9,12 @@ import './../node_modules/bootstrap/dist/js/bootstrap';
 import App from "./App";
 import createStore from './store';
 import {initializeAction} from "./actions/initialize";
+import {refreshAction} from "./actions/git/refresh";
 
 let store = createStore();
 
 store.dispatch(initializeAction());
+
+remote.app.on('browser-window-focus', () => store.dispatch(refreshAction()));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
